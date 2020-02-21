36968b4d0fef91b67c1cd87984c80199a5d69c98
Graf Stor <bratakmckey@gmail.com>
2020-02-22 00:41:30 +0300

    version 1

--- Lissajous Curve Table/main.py
+++ Lissajous Curve Table/main.py
@@ -15,7 +15,8 @@ class LineX:
         self.speed = speed
         self.begin_coords = begin_coords
         self.coords = begin_coords
-        self.direct = True
+        self.last_cord = self.coords
+        self.prom = False
         self.angle = 180
 
     def animate(self):
@@ -28,12 +29,17 @@ class LineX:
     def get(self):
         return self.coords
 
+    def get_last(self):
+        self.prom = True
+        return self.last_cord
+
 class LineY:
     def __init__(self, speed, begin_coords):
         self.speed = speed
         self.begin_coords = begin_coords
         self.coords = begin_coords + 35
-        self.direct = True
+        self.last_cord = self.coords
+        self.prom = False
         self.angle = 90
 
     def animate(self):
@@ -41,10 +47,15 @@ class LineY:
         acel_x = sin(radians(self.angle))*0.6*self.speed
 
         self.coords +=  acel_x
+
         return acel_x
 
     def get(self):
         return self.coords
+
+    def get_last(self):
+        # self.last_cord = self.coords
+        return self.last_cord
 
 class MainFrame:
     def __init__(self, root):
@@ -72,7 +83,9 @@ class MainFrame:
         self.canv_lines_x = []
         self.canv_lines_y = []
 
-        self.steps = 3
+        self.steps = 6
+        self.ii = 0
+        self.last_cord = (0,0)
 
         self.create_field()
 
@@ -104,18 +117,18 @@ class MainFrame:
             line = LineX(i + 1, step)
             self.lines_x.append(line)
 
-            # line = self.canvas.create_line(step, 0, step, self.win_size,
-            #                                fill=self.alt_color,
-            #                                width=1)
-            # self.canv_lines_x.append(line)
+            line = self.canvas.create_line(step, 0, step, self.win_size,
+                                           fill=self.alt_color,
+                                           width=1)
+            self.canv_lines_x.append(line)
 
             line = LineY(i + 1, step)
             self.lines_y.append(line)
 
-            # line = self.canvas.create_line(0, step+35, self.win_size, step+35,
-            #                                fill=self.alt_color,
-            #                                width=1)
-            # self.canv_lines_y.append(line)
+            line = self.canvas.create_line(0, step+35, self.win_size, step+35,
+                                           fill=self.alt_color,
+                                           width=1)
+            self.canv_lines_y.append(line)
 
         self.start_animation()
 
@@ -123,27 +136,40 @@ class MainFrame:
 
         for i in range(1000):
             self.animate()
+            self.ii+=1
             self.root.update()
-            sleep(0.01)
+            # sleep(0.0)
 
     def move_obj(self, obj,x=0,y=0):
         self.canvas.move(obj, x, y)
 
     def animate(self):
+        # for t in range(self.steps):
+
         for i in range(self.steps):
-            plus = self.lines_x[i].animate()
-            # self.move_obj(self.canv_lines_x[i], plus, 0)
-
             plus = self.lines_y[i].animate()
-            # self.move_obj(self.canv_lines_y[i], 0, plus)
-
-            for i in range(self.steps):
-                for j in range(self.steps):
+            self.move_obj(self.canv_lines_y[i], 0, plus)
+            plus = self.lines_x[i].animate()
+            self.move_obj(self.canv_lines_x[i], plus, 0)
+            for j in range(self.steps):
+                # if not self.ii%7:
+                #     line = self.canvas.create_line(cor_x, cor_y, cor_x+1, cor_y+1,
+                #                                fill=self.alt_color,
+                #                                width=1)
+                if not self.ii%4:
+                    ii = 0
                     cor_x = self.lines_x[j].get()
                     cor_y = self.lines_y[i].get()
+
+                    lastx = self.lines_x[j].get_last()
+                    lasty = self.lines_y[i].get_last()
+                    # line = self.canvas.create_line(lastx, lasty, cor_x, cor_y,
+                    #                        fill=self.alt_color,
+                    #                        width=1)
                     line = self.canvas.create_line(cor_x, cor_y, cor_x+1, cor_y+1,
-                                               fill=self.alt_color,
-                                               width=1)
+                           fill=self.alt_color,
+                           width=1)
+                    self.last_cord = (cor_x, cor_y)
 
 
 if __name__ == "__main__":
