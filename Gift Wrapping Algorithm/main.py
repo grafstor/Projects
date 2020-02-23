d8fb4537a0e34c3192b435af1ab75b2aa7876e5e
Graf Stor <bratakmckey@gmail.com>
2020-02-23 14:26:19 +0300

    not work yet

--- Gift Wrapping Algorithm/main.py
+++ Gift Wrapping Algorithm/main.py
@@ -9,9 +9,9 @@ __version__ = 1
 
 from tkinter import *
 from random import randint
-from math import radians, acos, asin, sqrt
+from math import radians, acos, cos, sin, sqrt, degrees
 from time import sleep
-import time
+# import time
 
 class MainFrame:
     def __init__(self, root):
@@ -20,6 +20,7 @@ class MainFrame:
 
         self.main_color = "black"
         self.alt_color = "white"
+        self.red_color = "red"
 
         self.root.geometry(f'{self.win_size}x{self.win_size}+{300}+{100}')
         self.root.title("Curves")
@@ -35,15 +36,16 @@ class MainFrame:
 
         self.dots_coords = []
 
-        self.dot_num = 30
+        self.dot_num = 10
 
         self.make_field()
 
-        self.nod = self.dots_coords[self.find_top()]
-        self.one(self.nod)
+        nod = self.dots_coords[self.find_top()]
+        self.first = nod
+        self.one(nod, 0)
 
     def make_field(self):
-        indent = self.win_size * 0.2
+        indent = self.win_size * 0.1
         right_bord = self.win_size - indent
 
         for i in range(self.dot_num):
@@ -53,22 +55,49 @@ class MainFrame:
             self.dots_coords.append((x+3, y+3))
 
 
-    def one(self, nod):
-        tt = time.time()
-        nx,xy = nod
+
+    def one(self, nod, angale):
+        self.dots_coords.pop(self.dots_coords.index(nod))
+        nx,ny = nod
+
+        ax = cos(radians(angale))*30
+        ay = sin(radians(angale))*30
+
+        self.create_line(nod, (ax+nx,ay+ny))
+        self.create_dot(nx-3, ny-3, self.red_color)
         array = []
 
-        for dot in self.dots_coords:
-            if dot != nod:
-                x,y = dot
-                x,y = (x - nx, y - xy)
-                z = sqrt(x**2 + y**2)
+        for ind in range(len(self.dots_coords)):
+            x,y = self.dots_coords[ind]
+            bx, by = (x - nx, -(y - ny))
 
-                angele = asin(y/z)
-                array.append(angele)
-                self.root.update()
-                
-        self.create_line(nod, self.dots_coords[array.index(min(array))])
+            txy = ax*bx + ay*by
+            sqtxy = sqrt(ax**2 + ay**2) * sqrt(bx**2 + by**2)
+
+            angele = acos(txy/sqtxy)
+
+            array.append(angele)
+
+            self.root.update()
+        # print(sort(array))
+
+        print(degrees(min(array)))
+
+        if len(self.dots_coords) == self.dot_num -1:
+            self.endl = self.dots_coords[array.index(max(array))]
+            self.create_dot(self.endl[0], self.endl[1], self.red_color)
+
+        sleep(1)
+
+        con = self.dots_coords[array.index(min(array))]
+
+        # if con == self.endl:
+        #     print(0)
+        #     self.create_line(self.endl, self.first)
+        # else:
+
+        self.create_line(nod, con)
+        self.one(con, degrees(min(array)))
 
     def find_top(self):
         x_list = []
@@ -80,12 +109,12 @@ class MainFrame:
 
         return y_list.index(min(y_list))
 
-    def create_dot(self, x, y):
+    def create_dot(self, x, y, col="white"):
         self.canvas.create_arc(0+x, 0+y, 6+x, 6+y, 
                                start=0,
                                extent=359,
-                               fill=self.alt_color,
-                               outline=self.alt_color,
+                               fill=col,
+                               outline=col,
                                width=1,
                                )
 
